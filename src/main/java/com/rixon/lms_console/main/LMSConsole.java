73bb4f561b57d64255d92808b43d36fcff7d5539
Rixon <rixonmathew@gmail.com>
2012-08-11 21:43:25 +0530

    minor changes to project files and LMSConsole

--- src/main/java/com/rixon/lms_console/main/LMSConsole.java
+++ src/main/java/com/rixon/lms_console/main/LMSConsole.java
@@ -1,5 +1,10 @@
 package com.rixon.lms_console.main;
 
+import com.rixon.lms_console.command.Command;
+import com.rixon.lms_console.commandbuilder.CommandBuilder;
+
+import java.io.Console;
+
 /**
  * Created with IntelliJ IDEA.
  * User: rixonmathew
@@ -9,10 +14,34 @@ package com.rixon.lms_console.main;
  */
 public class LMSConsole {
 
+    public static final String EXIT_COMMAND = "exit";
+    public static final String EXIT_MESSAGE = "Have a good day %n";
+
+    private static Command<String> createCommand(String commandString){
+      CommandBuilder builder = new CommandBuilder();
+      return builder.buildCommand(commandString);
+    }
+
+    private static void executeCommand(Command<String> command){
+
+    }
     public static void main(String[] args) {
         //TODO Create a console that displays the system and gets the command from the users
         //TODO Display the results of the command validation
         //TODO Display the results of the command
+        Console console = System.console();
+        String userCommandString="";
+        if (console!=null){
+            console.printf("****** Welcome to LMS ******.%n");
+            console.printf("-----------------------------.%n");
+            console.printf("(Type help to get help, exit|quit to quit the application)%n");
+            do{
+              userCommandString = console.readLine("LMS>");
+              console.printf("Command typed is: %1$4s%n",userCommandString);
+            }
+            while (!userCommandString.equalsIgnoreCase(EXIT_COMMAND));
+            console.printf(EXIT_MESSAGE);
+        }
 
     }
 }
