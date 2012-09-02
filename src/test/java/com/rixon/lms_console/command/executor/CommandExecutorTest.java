bf56e08fd9d285741df611cb2ca6bc0d3f947b7c
rixon <rixonmathew@gmail.com>
2012-09-02 12:52:08 +0530

    added test for Reserve command where parameters are invalid

--- src/test/java/com/rixon/lms_console/command/executor/CommandExecutorTest.java
+++ src/test/java/com/rixon/lms_console/command/executor/CommandExecutorTest.java
@@ -23,9 +23,9 @@ public class CommandExecutorTest extends AbstractCommandTest {
 
     @Before
     public void setUp() {
-      super.setup();
-      executor = new SimpleCommandExecutor();
-      System.setProperty(ServiceFacadeFactory.LMS_MODE,ServiceFacadeFactory.FAKE);
+        super.setup();
+        executor = new SimpleCommandExecutor();
+        System.setProperty(ServiceFacadeFactory.LMS_MODE, ServiceFacadeFactory.FAKE);
     }
 
     @Test
@@ -42,112 +42,124 @@ public class CommandExecutorTest extends AbstractCommandTest {
 
     @Test
     public void testHelpCommandExecution() {
-        Command helpCommand = CommandTestUtil.createCommand("help","",builder);
+        Command helpCommand = CommandTestUtil.createCommand("help", "", builder);
         Result result = executor.executeCommand(helpCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
         final int expected = 7;
-        assertEquals("Displayed count of commands is not same", expected,tableModel.getRowCount());
-        assertEquals("Displayed count of result columns is not same",1,tableModel.getColumnCount());
+        assertEquals("Displayed count of commands is not same", expected, tableModel.getRowCount());
+        assertEquals("Displayed count of result columns is not same", 1, tableModel.getColumnCount());
         assertCellValues(tableModel);
     }
 
     @Test
     public void testHelpCommandExecutionForOperation() {
-        Command helpCommand = CommandTestUtil.createCommand("help","search",builder);
+        Command helpCommand = CommandTestUtil.createCommand("help", "search", builder);
         Result result = executor.executeCommand(helpCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
-        assertEquals("Displayed count of commands is not same",1,tableModel.getRowCount());
-        assertEquals("Displayed count of result columns is not same",1,tableModel.getColumnCount());
+        assertEquals("Displayed count of commands is not same", 1, tableModel.getRowCount());
+        assertEquals("Displayed count of result columns is not same", 1, tableModel.getColumnCount());
         assertCellValues(tableModel);
     }
 
     @Test
     public void testIssueCommandExecution() {
-        Command issueCommand = CommandTestUtil.createCommand("issue","1123122 u100120",builder);
+        Command issueCommand = CommandTestUtil.createCommand("issue", "1123122 u100120", builder);
         Result result = executor.executeCommand(issueCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
-        assertEquals("Displayed rows for issue command is not same",1,tableModel.getRowCount());
+        assertEquals("Displayed rows for issue command is not same", 1, tableModel.getRowCount());
         assertCellValues(tableModel);
     }
 
     @Test
     public void testTransferCommandExecution() {
-        Command transferCommand = CommandTestUtil.createCommand("transfer","1123122 u100120 u112232",builder);
+        Command transferCommand = CommandTestUtil.createCommand("transfer", "1123122 u100120 u112232", builder);
         Result result = executor.executeCommand(transferCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
-        assertEquals("Displayed rows for issue command is not same",1,tableModel.getRowCount());
+        assertEquals("Displayed rows for issue command is not same", 1, tableModel.getRowCount());
         assertCellValues(tableModel);
     }
 
     @Test
     public void testReturnCommandExecution() {
-        Command returnCommand = CommandTestUtil.createCommand("return","1123122 u100120",builder);
+        Command returnCommand = CommandTestUtil.createCommand("return", "1123122 u100120", builder);
         Result result = executor.executeCommand(returnCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
-        assertEquals("Displayed rows for issue command is not same",1,tableModel.getRowCount());
+        assertEquals("Displayed rows for issue command is not same", 1, tableModel.getRowCount());
         assertCellValues(tableModel);
     }
 
     @Test
     public void testInvalidCommandExecution() {
-        Command invalidCommand = CommandTestUtil.createCommand("invalid","112312312,123123,12312312,123123",builder);
+        Command invalidCommand = CommandTestUtil.createCommand("invalid", "112312312,123123,12312312,123123", builder);
         Result result = executor.executeCommand(invalidCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
-        assertEquals("Displayed rows for invalid command is not same",2,tableModel.getRowCount());
+        assertEquals("Displayed rows for invalid command is not same", 2, tableModel.getRowCount());
         assertCellValues(tableModel);
     }
 
     @Test
     public void testExitCommandExecution() {
-        Command exitCommand = CommandTestUtil.createCommand("exit","",builder);
+        Command exitCommand = CommandTestUtil.createCommand("exit", "", builder);
         Result result = executor.executeCommand(exitCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
-        assertEquals("Displayed rows for invalid command is not same",1,tableModel.getRowCount());
+        assertEquals("Displayed rows for invalid command is not same", 1, tableModel.getRowCount());
         assertCellValues(tableModel);
     }
+
     @Test
     public void testExitCommandExecutionWithInvalidParameters() {
-        Command exitCommand = CommandTestUtil.createCommand("exit","112321123",builder);
+        Command exitCommand = CommandTestUtil.createCommand("exit", "112321123", builder);
         CommandTestUtil.assertValidationResult(exitCommand.getValidationResult(), false);
         Result result = executor.executeCommand(exitCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
-        assertEquals("Displayed rows for invalid command is not same",2,tableModel.getRowCount());
+        assertEquals("Displayed rows for invalid command is not same", 2, tableModel.getRowCount());
         assertCellValues(tableModel);
     }
 
     @Test
     public void testReserveCommand() {
-        Command reservedCommand = CommandTestUtil.createCommand("reserve","112321123 user123",builder);
+        Command reservedCommand = CommandTestUtil.createCommand("reserve", "112321123 user123", builder);
         CommandTestUtil.assertValidationResult(reservedCommand.getValidationResult(), true);
         Result result = executor.executeCommand(reservedCommand);
         assertNotNull(result);
         TableModel tableModel = result.getResultsTable();
         final int expectedMessageCount = 1;
-        assertEquals("Displayed rows for invalid command is not same", expectedMessageCount,tableModel.getRowCount());
+        assertEquals("Displayed rows for invalid command is not same", expectedMessageCount, tableModel.getRowCount());
+        assertCellValues(tableModel);
+    }
+
+    @Test
+    public void testReserveCommandWithInvalidParameters() {
+        Command reservedCommand = CommandTestUtil.createCommand("reserve", "112321123", builder);
+        CommandTestUtil.assertValidationResult(reservedCommand.getValidationResult(), false);
+        Result result = executor.executeCommand(reservedCommand);
+        assertNotNull(result);
+        TableModel tableModel = result.getResultsTable();
+        final int expectedMessageCount = 2;
+        assertEquals("Displayed rows for invalid command is not same", expectedMessageCount, tableModel.getRowCount());
         assertCellValues(tableModel);
     }
 
 
-
     private void assertCellValues(TableModel tableModel) {
 
-        for(int column=0;column<tableModel.getColumnCount();column++) {
+        for (int column = 0; column < tableModel.getColumnCount(); column++) {
             Object columnName = tableModel.getColumnName(column);
-            assertNotNull("Column name is null at "+column,columnName);
+            assertNotNull("Column name is null at " + column, columnName);
         }
 
-        for (int row=0;row<tableModel.getRowCount();row++) {
-            for (int column=0;column<tableModel.getColumnCount();column++){
-                Object cellValue = tableModel.getValueAt(row,column);
-                //assertNotNull("Cell value null at row:"+row+" column:"+column,cellValue);
+        for (int row = 0; row < tableModel.getRowCount(); row++) {
+            for (int column = 0; column < tableModel.getColumnCount(); column++) {
+                Object cellValue = tableModel.getValueAt(row, column);
+                assertNotNull("Cell value null at row:" + row + " column:" + column, cellValue);
             }
         }
     }
