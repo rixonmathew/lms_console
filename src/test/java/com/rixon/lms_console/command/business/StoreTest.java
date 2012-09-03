60b838e70ade47cc5e4ea41dee28935396d16618
rixon <rixonmathew@gmail.com>
2012-09-03 17:35:39 +0530

    Introduced ServiceTests for real mode

--- src/test/java/com/rixon/lms_console/command/business/StoreTest.java
+++ src/test/java/com/rixon/lms_console/command/business/StoreTest.java
@@ -9,8 +9,8 @@ package com.rixon.lms_console.command.business;
 
 import com.rixon.lms_console.business.SimpleStore;
 import com.rixon.lms_console.business.Store;
+import com.rixon.lms_console.command.CommandTestUtil;
 import com.rixon.lms_console.command.Parameter;
-import com.rixon.lms_console.command.SimpleParameter;
 import com.rixon.lms_console.command.result.Result;
 import com.rixon.lms_console.dao.SearchQuery;
 import org.junit.After;
@@ -18,8 +18,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.swing.table.TableModel;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -50,18 +48,13 @@ public class StoreTest {
         Result searchResults = store.searchItemByTitle(searchQuery);
         assertNotNull(searchResults);
         TableModel tableModel = searchResults.getResultsTable();
-        final int expectedRowCount = 5;
+        final int expectedRowCount = 8;
         assertEquals("Row count is not as expected", expectedRowCount, tableModel.getRowCount());
 
     }
 
     private SearchQuery getSearchQueryWithParameters(boolean anyFieldMatch, String... searchTexts) {
-
-        List<String> texts = new ArrayList<String>();
-        for (String title : searchTexts) {
-            texts.add(title);
-        }
-        Parameter<String> simpleParameter = new SimpleParameter(texts);
+        Parameter<String> simpleParameter = CommandTestUtil.parameterWithString(searchTexts);
         SearchQuery searchQuery = new SearchQuery(simpleParameter, anyFieldMatch);
         return searchQuery;
     }
